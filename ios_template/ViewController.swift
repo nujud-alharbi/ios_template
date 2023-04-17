404fe0a3a8f09bef16244f316a3637ddf6ae8851
nujudalalawi <njoud.aali.med.s@tuwaiq.edu.sa>
2023-04-17 02:54:46 -0700

    Initial Commit

---
+++ ios_template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_template
+//
+//  Created by Nujud Alalawi on 26/09/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
