f323dc8519fe9c6344baa95ab62eabe81274bbec
nujudalalawi <njoud.aali.med.s@tuwaiq.edu.sa>
2023-04-30 13:41:45 -0700

    feat: add localization in app

---
+++ ios_template/core/localization/logic/language.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  ios_template
+//
+//  Created by Nujud Alalawi on 10/10/1444 AH.
+//
+
+import UIKit
+class Language {
+    
+
+    
+    
+    
+//    save current language
+    func changeLanguage (){
+        
+        let currentLang =  Locale.current.languageCode
+        let newLang = currentLang == "en" ? "ar" : "en"
+        UserDefaults.standard.setValue([newLang], forKey: "AppleLanguages")
+        
+        exit(0)
+    }
+}
